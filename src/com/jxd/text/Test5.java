34a540870225f5300f7210d3b063cc09f202968a
jxd <jxd@DESKTOP-BHOTDEM.mshome.net>
2020-07-18 21:37:23 +0800

    合并分支代码

--- src/com/jxd/text/Test5.java
+++ src/com/jxd/text/Test5.java
@@ -3,6 +3,7 @@ package com.jxd.text;
 public class Test5 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world dev1");
 		System.out.println("hello world master1");
 	}
 }
