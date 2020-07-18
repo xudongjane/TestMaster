d8b42e691e297543688dbaee757334c659b796c4
jxd <jxd@DESKTOP-BHOTDEM.mshome.net>
2020-07-18 19:39:34 +0800

    dev,提交2

--- src/com/jxd/text/Test.java
+++ src/com/jxd/text/Test.java
@@ -4,4 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
+	
+	public String name2() {
+		return "dev";
+	}
 }
--- src/com/jxd/text/Test3.java
+++ src/com/jxd/text/Test3.java
@@ -3,6 +3,7 @@ package com.jxd.text;
 public class Test3 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world dev");
 	}
 	
 	public String name() {
