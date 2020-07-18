51336b75cc5395638f9eac4468168c7e56942304
jxd <jxd@DESKTOP-BHOTDEM.mshome.net>
2020-07-18 19:33:33 +0800

    dev,提交1

--- src/com/jxd/text/Test4.java
+++ src/com/jxd/text/Test4.java
@@ -1,11 +1,7 @@
 package com.jxd.text;
 
-public class Test3 {
+public class Test4 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-	}
-	
-	public String name() {
-		return "";
 	}
 }
