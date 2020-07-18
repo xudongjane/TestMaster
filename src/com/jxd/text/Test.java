d2081702c186f571df62edeb4e2f6cca2757867e
jxd <jxd@DESKTOP-BHOTDEM.mshome.net>
2020-07-18 21:14:58 +0800

    master 21:14

--- src/com/jxd/text/Test.java
+++ src/com/jxd/text/Test.java
@@ -3,6 +3,7 @@ package com.jxd.text;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world master1");
 	}
 	
 	public String name2() {
