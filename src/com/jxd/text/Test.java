86bc0b9f63fd73b72580b8bf3519d3a79727eedc
jxd <jxd@DESKTOP-BHOTDEM.mshome.net>
2020-07-18 21:17:39 +0800

    dev 21:17

--- src/com/jxd/text/Test.java
+++ src/com/jxd/text/Test.java
@@ -3,6 +3,7 @@ package com.jxd.text;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world dev1");
 	}
 	
 	public String name2() {
--- src/com/jxd/text/Test4.java
+++ src/com/jxd/text/Test4.java
@@ -3,5 +3,6 @@ package com.jxd.text;
 public class Test4 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world dev1");
 	}
 }
